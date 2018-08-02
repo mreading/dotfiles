a3d57c309c3dfc6e15671b1c43e8283b3c16c3df
mreading <mreading1998@gmail.com>
2018-08-02 00:30:44 -0400

    add directory colors

--- mac/macos.sh
+++ mac/macos.sh
@@ -116,7 +116,7 @@ defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
 defaults write com.apple.terminal StringEncodings -array 4
 
 # Install the Solarized Dark theme for iTerm
-open "${PWD}/init/SolarizedDark.itermcolors"
+open "${PWD}/mac/init/SolarizedDark.itermcolors"
 
 # Don’t display the annoying prompt when quitting iTerm
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
